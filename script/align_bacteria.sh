bf9140c99228a9dc1b058eb05618efcfada00ed7
Shenwei <692949389@QQ.com>
2022-11-25 16:56:06 +0800

    Update align_bacteria.sh

--- script/align_bacteria.sh
+++ script/align_bacteria.sh
@@ -1,14 +1,12 @@
 #! usr/bin/bash
-for TISSUE in flower stem leaf;do
-  for PREFIX in Ath_${TISSUE}_NC Ath_${TISSUE}_1 Ath_${TISSUE}_2 Ath_${TISSUE}_3;do
-    THREAD=24
-      find ASSEMBLY -maxdepth 1 -mindepth 1 -type d |
-      cut -d "/" -f 2 |
-      parallel -j 12 --line-buffer "
+for PREFIX in Ath_flower_1 Ath_flower_2 Ath_flower_3;do
+  find ASSEMBLY -maxdepth 1 -mindepth 1 -type d |
+    cut -d "/" -f 2 |
+      parallel -j 20 --line-buffer "
         echo '===>{1}'
         mkdir -p output/${PREFIX}/{1}
 
-        bowtie2 -p ${THREAD} -a -t \
+        bowtie2 -a -t \
           --end-to-end -D 20 -R 3 \
           -N 0 -L 10 -i S,1,0.50 --np 0 \
           --xeq -x index/{1}/{1} \
@@ -17,7 +15,6 @@ for TISSUE in flower stem leaf;do
           2>&1 |
         tee output/${PREFIX}/{1}/{1}.bowtie2.log
 
-        pigz -p ${THREAD} output/${PREFIX}/{1}/{1}_align.sam
+        pigz output/${PREFIX}/{1}/{1}_align.sam
       "
-  done
 done
