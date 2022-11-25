544f1f72cf8bb1499eeb0726c5cd4a6bcd397220
Shenwei <692949389@QQ.com>
2022-11-25 16:56:09 +0800

    Update merge_plot.sh

--- script/merge_plot.sh
+++ script/merge_plot.sh
@@ -1,18 +1,21 @@
 #! usr/bin/bash
-PREFIX=Ath_root_NC
+PREFIX=Ath_flower_NC
 
 JOB=$(ls output/${PREFIX})
 
-echo -e "Bacteria_name\tTotal_reads_number\tMatch_reads_number\tMatch_proportion\tAverage_CG_content" > output/${PREFIX}/align_statistics.tsv
+echo -e "name\tTotal_reads_number\tMatch_reads_number\tMatch_proportion\tAverage_CG_content\tAverage_reads_Length" > output/${PREFIX}/align_statistics.tsv
+
 for J in $JOB;do
   echo "====> $J"
 
   cat output/${PREFIX}/$J/remove_info.tsv |  
-    datamash transpose > output/${PREFIX}/$J/remove_info.tsv
+    datamash transpose > output/${PREFIX}/$J/tem&&
+    mv output/${PREFIX}/$J/tem output/${PREFIX}/$J/remove_info.tsv
 
   # strain name
   (echo -e "bacteria_name\t$J" && cat output/${PREFIX}/$J/remove_info.tsv) > output/${PREFIX}/$J/tem &&
     mv output/${PREFIX}/$J/tem output/${PREFIX}/$J/remove_info.tsv
+
   # Average CG content
   CG_AVERAGE=$(cat output/${PREFIX}/$J/reads_info.tsv | perl -e'
               my $total_CG;
@@ -28,8 +31,24 @@ for J in $JOB;do
               ')
   echo -e "Average_CG_content\t$CG_AVERAGE" >> output/${PREFIX}/$J/remove_info.tsv
 
+  # Average reads length
+  length_AVERAGE=$(cat output/${PREFIX}/$J/reads_info.tsv | perl -e'
+                my $total_length;
+                my $n;
+                while (<>) {
+                  if (/\S+\t\S+\t(\S+)\t\S+/) {
+                    $total_length = $total_length + $1;
+                    $n++;
+                  }
+                }
+                my $length_average = ( $total_length / $n );
+                printf "%.2f", $length_average;
+                ')
+  echo -e "Average_reads_length\t$length_AVERAGE" >> output/${PREFIX}/$J/remove_info.tsv
+
   cat output/${PREFIX}/$J/remove_info.tsv |  
-    datamash transpose > output/${PREFIX}/$J/remove_info.tsv
+    datamash transpose > output/${PREFIX}/$J/tem&&
+    mv output/${PREFIX}/$J/tem output/${PREFIX}/$J/remove_info.tsv
 
   # Length distribution
   (echo -e "Reads_length\tNumber" && cut -f 3 output/${PREFIX}/$J/reads_info.tsv | sort -n |
@@ -37,7 +56,7 @@ for J in $JOB;do
       s/^\s+//;
       print "$_";
     ' | tr " " "\t" |
-    tsv-select --fields 2,1) > output/${PREFIX}/$J/length.tsv
+    tsv-select --fields 2,1) > output/${PREFIX}/$J/reads_length_distribution.tsv
 
   # merge
   sed '1d' output/${PREFIX}/$J/remove_info.tsv >> output/${PREFIX}/align_statistics.tsv
