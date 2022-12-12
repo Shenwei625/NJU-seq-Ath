b4494b379441c7d538ecb160f17cb590f89cabbb
Shenwei <692949389@QQ.com>
2022-12-12 16:54:35 +0800

    Update align_bacteria.sh

--- script/align_bacteria.sh
+++ script/align_bacteria.sh
@@ -1,20 +1,22 @@
 #! usr/bin/bash
-for PREFIX in Ath_flower_1 Ath_flower_2 Ath_flower_3;do
-  find ASSEMBLY -maxdepth 1 -mindepth 1 -type d |
-    cut -d "/" -f 2 |
+for PREFIX in Ath_flower_NC Ath_flower_1 Ath_flower_2 Ath_flower_3;do
+mkdir -p rRNA_tRNA/output/${PREFIX}
+
+find index/rRNA_tRNA -maxdepth 1 -mindepth 1 -type d |
+    cut -d "/" -f 3 |
       parallel -j 20 --line-buffer "
-        echo '===>{1}'
-        mkdir -p output/${PREFIX}/{1}
+        echo '===>$PREFIX {1}'
+        mkdir -p rRNA_tRNA/output/${PREFIX}/{1}
 
         bowtie2 -a -t \
           --end-to-end -D 20 -R 3 \
           -N 0 -L 10 -i S,1,0.50 --np 0 \
-          --xeq -x index/{1}/{1} \
-          -1 ../data/${PREFIX}/R1.fq.gz -2 ../data/${PREFIX}/R2.fq.gz \
-          -S output/${PREFIX}/{1}/{1}_align.sam \
+          --xeq -x index/rRNA_tRNA/{1}/{1} \
+          -1 rRNA_conserve/filter/${PREFIX}/R1_conserve.fq.gz -2 rRNA_conserve/filter/${PREFIX}/R2_conserve.fq.gz \
+          -S rRNA_tRNA/output/${PREFIX}/{1}/{1}_align.sam \
           2>&1 |
-        tee output/${PREFIX}/{1}/{1}.bowtie2.log
+        tee rRNA_tRNA/output/${PREFIX}/{1}/{1}.bowtie2.log
 
-        pigz output/${PREFIX}/{1}/{1}_align.sam
+        pigz rRNA_tRNA/output/${PREFIX}/{1}/{1}_align.sam
       "
 done
