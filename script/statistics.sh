7fe80b5c86b4f20b01d77c1261198d05557e7f5b
Shenwei <692949389@QQ.com>
2022-12-12 16:54:21 +0800

    Update statistics.sh

--- script/statistics.sh
+++ script/statistics.sh
@@ -1,17 +1,19 @@
 #! usr/bin/bash
-PREFIX=Ath_flower_NC
+# bash statistics.sh dir PREFIX
 
-ls ./output/${PREFIX} |
-  parallel -j 24 --linebuffer '
-  echo "====> {1}"
+PREFIX=$2
 
-  pigz -dcf ./output/Ath_flower_NC/{1}/{1}_align.sam.gz |
+ls $1/output/${PREFIX} |
+  parallel -j 20 --linebuffer "
+  echo '====> {1}'
+
+  pigz -dcf $1/output/${PREFIX}/{1}/{1}_align.sam.gz |
         grep -v "@" |
         tsv-filter --regex '6:^[0-9]+=$' |
         tsv-filter --str-eq 7:= |
-        cut -f 1,10 | uniq > ./output/Ath_flower_NC/{1}/{1}_match.tsv
+        cut -f 1,10 | tsv-uniq -f 1 > $1/output/${PREFIX}/{1}/{1}_match.tsv
 
-  perl script/pre_remove_statistics.pl ../data/Ath_flower_NC/R1.fq.gz output/Ath_flower_NC/{1}/{1}_match.tsv \
-  output/Ath_flower_NC/{1}/reads_info.tsv \
-  output/Ath_flower_NC/{1}/remove_info.tsv
-'
+  perl script/pre_remove_statistics.pl ../data/${PREFIX}/R1.fq.gz $1/output/${PREFIX}/{1}/{1}_match.tsv \
+  $1/output/${PREFIX}/{1}/reads_info.tsv \
+  $1/output/${PREFIX}/{1}/remove_info.tsv
+"
